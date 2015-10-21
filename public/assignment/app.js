005369c12f4dd92ea228ad4430b03768f6cc3a1c
Praneeta Jhanwar <praneeta@ccs.neu.edu>
2015-10-20 17:56:02 -0700

    Using ng-include for header

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,5 +1,4 @@
-(function()
-{
+(function(){
 	angular
-		.module("FormMakerApp", ["ngRoute"]);	
+		.module("FormBuilderApp", ["ngRoute"]);	
 })();
---
+++ public/assignment/config.js
@@ -0,0 +1,8 @@
+(function(){
+	angular
+		.module("FormBuilderApp")
+		.config(Configure);
+		
+	function Configure($routeProvider) {
+	}
+}) ();
