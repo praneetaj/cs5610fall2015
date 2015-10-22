be34cb27e33b5fc8c98fc525f42845e408fa5e4d
Praneeta Jhanwar <praneeta@ccs.neu.edu>
2015-10-22 03:05:45 -0700

    Implemented Sidebar controller and header controller

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -4,5 +4,27 @@
 		.config(Configure);
 		
 	function Configure($routeProvider) {
+		$routeProvider
+			.when("/home", {
+				templateUrl: "home/home.html"
+			})
+			.when("/profile", {
+				templateUrl: "profile/profile.html"
+			})
+			.when("/admin", {
+				templateUrl: "admin/admin.html"
+			})
+			.when("/forms", {
+				templateUrl: "forms/forms.html"
+			})
+			.when("/register", {
+				templateUrl: "register/register.html"
+			})
+			.when("/login", {
+				templateUrl: "login/login.html"
+			})
+			.otherwise({
+				redirectTo: "/home"
+			});
 	}
 }) ();
