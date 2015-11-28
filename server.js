2ca26c34535118628a2072c62ec6a50ae80db82f
Praneeta Jhanwar <praneeta@ccs.neu.edu>
2015-11-28 03:47:36 -0800

    refactored server.js for openshift

--- server.js
+++ server.js
@@ -12,13 +12,26 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610fall2015exmpl1';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+
+/*
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/cs5610';
 console.log(mongo_url);
 mongoose.connect(mongo_url);
-var db = mongoose.connection;
+var db = mongoose.connection;  */
 
 require("./public/experiments/nodejs/angularjsClient/server.js")(app);
 require("./public/experiments/angular/jsonp/server/services/MovieService.js")(app);
