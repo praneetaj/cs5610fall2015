7c9f775fe9814dfd51a788164f0f47e26119009a
Praneeta Jhanwar <praneeta@ccs.neu.edu>
2015-12-09 00:07:21 -0800

    Option to create coupons for non existent menu items

--- public/project/client/views/restaurantCoupons/createCoupon.controller.js
+++ public/project/client/views/restaurantCoupons/createCoupon.controller.js
@@ -113,6 +113,13 @@
 
         function createQuantityCoupon () {
             if (validQuantityEntries()) {
+                //var itemName;
+                //if (typeof model.newCoupon.itemName == "undefined" || model.newCoupon.itemName == "") {
+                //    console.log("menu item not selected");
+                //    itemName = model.newCoupon.itemNameOther;
+                //}
+                //else
+                //    itemName = model.newCoupon.itemName;
                 var newCoupon = {
                     label : model.newCoupon.label,
                     couponType : "QUANTITY",
@@ -126,6 +133,7 @@
                     expiry : model.newCoupon.expiry
                 };
                 console.log(newCoupon);
+
                 LoyalUCouponService.addCouponForRest($rootScope.loggedInUser.restLocuId, newCoupon).then(function (response) {
                     alert("Coupon created successfully!");
                     console.log(response);
